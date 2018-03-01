39c16379348988516f87e822e9cae31f19566afc
veljkodjokic <veledjokic69@gmail.com>
2018-03-02 00:49:41 +0100

    dd

--- resources/views/jezero.blade.php
+++ resources/views/jezero.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row" style="background-color: transparent;">
-            <div class="panel panel-default" style="background-color: transparent; border-color: transparent;">
-                <div class="panel-heading" style="background-color: transparent;"><img src="pics/page-header.jpg" style="width: 100%; max-height: 315px;"></div><br>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<h3>GDE SE NALAZI TRESETI&Scaron;TE?</h3>
 
-                    JEZERO       
-                </div>
-            </div>
-        </div>
-</div>
+<p>Jezero Treseti&scaron;te se nalazi u Evropi, regionu Balkana, Srbiji, Vojvodini. Geografski položaj i &scaron;truktura putne mreže omogućava brzi dolazak do jezera! U tipičnom geografskom području Subotičko - Horgo&scaron;ke pesčare, na samom severu regiona vojvodine, nedaleko od mesta Palić, nalazi se jezero Treseti&scaron;te. Ne vidja se tako često, perfektan sklop prirodnih fenomena, 100 godi&scaron;nja borova suma sa jedne strane i druge pe&scaron;čano stepski krajolik ižmedju kojeg je sme&scaron;ten, pravi biser</p>
+
+<h2>JEZERO TRESETI&Scaron;TE</h2>
+
+<p>Koje sa pravom dobija &scaron;tatus za&scaron;tićenih prirodnih resursa drzave Srbije, a isto tako se tretira i u Evropskim mapama.</p>
+
+<p>Jezero lezi u pravcu <strong>severo - zapad, jugo - istok</strong>, uz samu granicu sa državom Mađarskom. Povrsine 5ha, prosečnom <strong>dubinom 2,20 - 2,60m</strong>, izlazeći iz glavnog korita dubina se smanjuje, &scaron;to je i odlika za ravničarska jezera. Dno jezera je blago muljevito zastupljen je pretežno tresetni mulj, koji nije &scaron;tetan za zivotinjski svet u vodi, nego je stani&scaron;te za razne, sitne organizme, koje predstavljaju prirodni izvor hrane. U jezeru dominira kvalitetna &scaron;aranska riba, koja svojom veličinom i borbeno&scaron;ću ne ostavlja ravnodusne, čak ni naj probirljivije &Scaron;arandžije! Pogledajte foto galiriju sa slikama o jezeru i okoline jezera.</p>
+
+<h3>KAKO DO JEZERA?</h3>
+
+<p>GPS koordinate jezera su: Latitude:&nbsp; 46&deg; 8&#39;55.00&quot;N, Longitude: 19&deg;45&#39;21.00&quot;E</p>
+
+<p><a href="https://tresetiste.com/mapa-do-jezera.jpg"><img src="https://tresetiste.com/mapa-do-jezera.jpg" style="margin-left:auto; margin-right:auto; width:700px" /></a></p>
 
 @endsection
--- resources/views/pravilnik.blade.php
+++ resources/views/pravilnik.blade.php
@@ -1,22 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row" style="background-color: transparent;">
-            <div class="panel panel-default" style="background-color: transparent; border-color: transparent;">
-                <div class="panel-heading" style="background-color: transparent;"><img src="pics/page-header.jpg" style="width: 100%; max-height: 315px;"></div><br>
+<style type="text/css">
+    body{
+        color: black
+    }
+</style>
+                    <p>Jezero poseduje kapitalne primerke &scaron;arana koje smo sa velikom pažnjom pu&scaron;tali u jezero i samim tim potezom omogućili svim posetiocima ulov možda i najvećeg &scaron;arana tkz &ldquo;RIBE ŽIVOTA&rdquo;.<br />
+Iz tog razloga su proistekle:</p>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<h3><b>Op&scaron;te odredbe(pravila pona&scaron;anja)</b></h3>
 
-                    PRAVILNIK       
-                </div>
-            </div>
-        </div>
-</div>
+<p>- Po dolasku na jezero posetioci se prijavljuju dežurnom čuvaru(volonteru), obezbeđenje je prisutno na jezeru <strong>24h</strong> tako da nije problem u bilo kojem vremenskom periodu pristupiti ribolovu.<br />
+- Po prijavi i posle dogovaranja sa čuvarom o ranije okupiranoj pozicij, dozvoljen je odlazak na poziciju sa ličnim prevozom, istovar stvari i zatim povratak vozila na oređeni čuvani parking.</p>
+
+<p>&nbsp;</p>
+
+<p>- Ukoliko ribolovac neposeduje du&scaron;ek za polaganje ribe može ga (iznajmiti) preuzeti kod čuvara pre početka ribolova.</p>
+
+<p>&nbsp;</p>
+
+<p>- Osnovni sistemi montaže moraju biti klizni ili polu fiksni zbog mogućeg oslobadjanja ribe ukoliko dođe do pucanja ili kidanja osnovnog najlona a sve u cilju za&scaron;tite ribe koja odlazi samo sa udicom i parčetom najlona a ne i sa celim sistemom i olovom, takođe i sok predvezi nisu poželjni iz istih razloga. Zbog prepreka u vodi koje smo postavili, moguće je da dođe do takvih neželjenih momenata.<br />
+- Dozvoljeno je kori&scaron;ćenje 3 &scaron;tapa sa pojednom udicom na kraju isključivo dubinskim sistemom.</p>
+
+<p>&nbsp;</p>
+
+<p>- Ribolov se vr&scaron;i isključivo sa platformi koje brojevima označene iza kojih postoji prostor (BOX) dimenzija 6x6 metara (negde i vi&scaron;e) sa dovoljnim prostorom za postavljanje &scaron;atora i dodatne opreme kako bi vi&scaron;e dnevni odmor bio prijatniji bez nepotrebnih inprovizacija.</p>
+
+<p>&nbsp;</p>
+
+<p>- Kako bi prijatan boravak i ponuda poseticima jezera bila kompletna, mogućnost kori&scaron;ćenja sanitarnog čvora i mogućnost tu&scaron;-kabina sa toplom vodom im stoji na raspolaganju, koji se nalazi u skolpu restorana<br />
+- Nakon izvlačenja ribe, riba se <strong>OBAVEZNO ODLAŽE NA VLAŽAN DU&Scaron;EK,</strong> tretira anti biotikom i nakon fotografisanja neozleđena vraća u vodu</p>
+
+<p>&nbsp;</p>
+
+<p>Ukoliko ribolovac želi da svoj ulov ovekoveči sa fotografijom i to je omogućeno, u tom slučaju kapitalna riba se mora ostaviti u <strong>&scaron;aranski SAK</strong> koji je određen za čuvanje ribe na dubini min 1,20 m, do fotografisanja.</p>
+
+<p>&nbsp;</p>
+
+<p><strong>Naj strožije će se kazniti lice koje na bilo kakav način (slučajno ili namerno)ugrozi život ribe,svojim nepravilnim pona&scaron;anjem i odnosom prema samoj ribi.</strong><br />
+<br />
+- Sve ove odredbe su sačinjene iz razloga očuvanja ribljeg fonda i humanijeg pristupa sportskom ribolovu a i samoj prirodi koja nas okružuje.</p>
+
+<p>JEZERO TRESETI&Scaron;TE pruža nezaboravne trenutke u ribolovu, zato smo se pobrinuli kroz vi&scaron;egodi&scaron;nje iskustvo i težnjom za kvalitetnijom turističko-ribolovnom uslugom.</p>      
 
 @endsection
