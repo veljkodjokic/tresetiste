8532bee9b017da21bf80aeba29d767b30b01c9f3
veljkodjokic <veledjokic69@gmail.com>
2018-03-02 02:03:12 +0100

    dd

--- resources/views/istorija/2006.blade.php
+++ resources/views/istorija/2006.blade.php
@@ -1,22 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row" style="background-color: transparent;">
-            <div class="panel panel-default" style="background-color: transparent; border-color: transparent;">
-                <div class="panel-heading" style="background-color: transparent;"><img src="/pics/page-header.jpg" style="width: 100%; max-height: 315px;"></div><br>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    ISTORIJA >> 2006      
-                </div>
-            </div>
-        </div>
-</div>
 
 @endsection
