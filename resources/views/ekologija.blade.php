2be8bbc7a5d578f00e1ad63422c7784d4458127b
veljkodjokic <veledjokic69@gmail.com>
2018-03-02 01:46:38 +0100

    dd

--- resources/views/ekologija.blade.php
+++ resources/views/ekologija.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row" style="background-color: transparent;">
-            <div class="panel panel-default" style="background-color: transparent; border-color: transparent;">
-                <div class="panel-heading" style="background-color: transparent;"><img src="pics/page-header.jpg" style="width: 100%; max-height: 315px;"></div><br>
+<style type="text/css">
+    body{
+        color: black;
+    }
+</style>
+<p>Kada se počelo sa sanacijom samog jezera sa upravom kluba se usaglasio stav da se jezero MORA pridržavati ekolo&scaron;kih normi pona&scaron;anja i ako je moguće iz na&scaron;eg ugla gledanja i usavr&scaron;avati.<br />
+Nakon izvučene velike količine mulja na obalu jezera koji je u velikoj meri ugrožavao opstanak svih stanovnika jezera,zbog svog raspadanja I stvaranja &scaron;tetnih gasova, počelo je sa &scaron;irenjem ostataka oko priobalja I odmah nakon toga se pristupilo sa ozelenjavanjem I sadnjom ISKLJUČIVO onim vrstama drveća koji su prepoznatljivi za ovo podnevlje nikako nije dolazilo u obzir da se posadi bilo kakva biljka koja nije tipična za ovaj Vojvođanski krajolik.Od drveća je zastupljena u90% vrba koja je naj autentičnija kao drvo koje raste uz jezera ,bare i močvare.</p>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<p>Takođe i priobalje kao i sama obala je dobila svoje stanovnike u narodu takozvani ČUVAR MOSTA ,LOKVANJ, ŽUTI BARSKI LJILJAN itd. Sve ove biljke smo brižno presađivali sa drugih lokacija ne ugrožavajući sam izvorni deo odakle je uzet ,naravno oni su takođe pod posebnim režimom koji se čuva i nesme se ugrožavati.</p>
 
-                    EKOLOGIJA
-                </div>
-            </div>
-        </div>
-</div>
+<p>&nbsp;<a href="https://tresetiste.com/pics/ekologija10lokvanj.jpg"><img src="https://tresetiste.com/pics/ekologija10lokvanj.jpg" style="height:138px" /></a> <a href="https://tresetiste.com/pics/ekologija4.jpg"><img src="https://tresetiste.com/pics/ekologija4.jpg" style="height:138px" /></a><a href="https://tresetiste.com/pics/ekologija8a.jpg"><img src="https://tresetiste.com/pics/ekologija8a.jpg" style="height:138px" /></a></p>
+
+<p>&nbsp;</p>
+
+<p>Ribe koje su nastanjene u jezeru može se reći da su za&scaron;tićene kao i ljudi, od vrsta koje su pod posebnim režimom čuvanja su: LINJAK, ZLATNI (BARSKI) KARA&Scaron;, ČIKOV kojima inače preti istrebljenje na otvorenim vodama i trebali bi da se stave pod za&scaron;titu jer su tipične ribe na&scaron;eg podnevlja. Mi smo to već uradili da bi mogli i budućim generacijama to preneti tako da svaki slučajno ulovljeni primerak mora sa veliko pažnjom da se vrati u jezero.</p>
+
+<p><a href="https://tresetiste.com/pics/zuti_karas.jpg"><img src="https://tresetiste.com/pics/zuti_karas.jpg" style="height:138px" /></a> <a href="https://tresetiste.com/pics/linjak.jpg"><img src="https://tresetiste.com/pics/linjak.jpg" style="height:138px" /></a> <a href="https://tresetiste.com/pics/linjak1.jpg"><img src="https://tresetiste.com/pics/linjak1.jpg" style="height:138px" /></a></p>
+
+<p>O žabama, gu&scaron;terima, zmijama i kornjačama koje su takođe na&scaron;i stanovnici brinemo i njihova stani&scaron;ta ne ugrožavamo kako bi se dovelo do tipičnog i pravog prirodnog lanca u ovakvoj mikro lokaciji koja se dalje nastavlja u projekat koji je inače pod za&scaron;titom i uz samu granicu sa Mađarskom se proteže kilometrima kojom je upravljač J.P Palić &ndash; Luda&scaron; i u saradnji sa stručnim &scaron;tabom koji nam pomaže oko boljeg upoznavanja oblasti Ekologije kao naučne grane u kojoj smo mi jo&scaron; pioniri a težnja nam je da se bolje upoznamo sa tom obasti koja je sastavni deo prirodnog lanca na jezeru.</p>
 
 @endsection
