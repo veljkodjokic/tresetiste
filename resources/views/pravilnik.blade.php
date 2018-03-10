ec7d985d17bfcfa8616e37bd851d8a37a30cd7ad
veljkodjokic <veledjokic69@gmail.com>
2018-03-10 16:46:35 +0100

    Ispravke pravilnika

--- resources/views/pravilnik.blade.php
+++ resources/views/pravilnik.blade.php
@@ -12,7 +12,7 @@ Iz tog razloga su proistekle:</p>
 <h3><b>Op&scaron;te odredbe(pravila pona&scaron;anja)</b></h3>
 
 <p>- Po dolasku na jezero posetioci se prijavljuju dežurnom čuvaru(volonteru), obezbeđenje je prisutno na jezeru <strong>24h</strong> tako da nije problem u bilo kojem vremenskom periodu pristupiti ribolovu.<br />
-- Po prijavi i posle dogovaranja sa čuvarom o ranije okupiranoj pozicij, dozvoljen je odlazak na poziciju sa ličnim prevozom, istovar stvari i zatim povratak vozila na oređeni čuvani parking.</p>
+- Po prijavi i posle dogovaranja sa čuvarom o ranije okupiranoj poziciji, dozvoljen je odlazak na poziciju sa ličnim prevozom, istovar stvari i zatim povratak vozila na određeni čuvani parking.</p>
 
 <p>&nbsp;</p>
 
@@ -20,17 +20,17 @@ Iz tog razloga su proistekle:</p>
 
 <p>&nbsp;</p>
 
-<p>- Osnovni sistemi montaže moraju biti klizni ili polu fiksni zbog mogućeg oslobadjanja ribe ukoliko dođe do pucanja ili kidanja osnovnog najlona a sve u cilju za&scaron;tite ribe koja odlazi samo sa udicom i parčetom najlona a ne i sa celim sistemom i olovom, takođe i sok predvezi nisu poželjni iz istih razloga. Zbog prepreka u vodi koje smo postavili, moguće je da dođe do takvih neželjenih momenata.<br />
+<p>- Osnovni sistemi montaže moraju biti klizni ili polu fiksni zbog mogućeg oslobađanja ribe ukoliko dođe do pucanja ili kidanja osnovnog najlona a sve u cilju za&scaron;tite ribe koja odlazi samo sa udicom i parčetom najlona a ne i sa celim sistemom i olovom, takođe i sok predvezi nisu poželjni iz istih razloga. Zbog prepreka u vodi koje smo postavili, moguće je da dođe do takvih neželjenih momenata.<br />
 - Dozvoljeno je kori&scaron;ćenje 3 &scaron;tapa sa pojednom udicom na kraju isključivo dubinskim sistemom.</p>
 
 <p>&nbsp;</p>
 
-<p>- Ribolov se vr&scaron;i isključivo sa platformi koje brojevima označene iza kojih postoji prostor (BOX) dimenzija 6x6 metara (negde i vi&scaron;e) sa dovoljnim prostorom za postavljanje &scaron;atora i dodatne opreme kako bi vi&scaron;e dnevni odmor bio prijatniji bez nepotrebnih inprovizacija.</p>
+<p>- Ribolov se vr&scaron;i isključivo sa platformi koje su brojevima označene iza kojih postoji prostor (BOX) dimenzija 6x6 metara (negde i vi&scaron;e) sa dovoljnim prostorom za postavljanje &scaron;atora i dodatne opreme kako bi vi&scaron;e dnevni odmor bio prijatniji bez nepotrebnih improvizacija.</p>
 
 <p>&nbsp;</p>
 
-<p>- Kako bi prijatan boravak i ponuda poseticima jezera bila kompletna, mogućnost kori&scaron;ćenja sanitarnog čvora i mogućnost tu&scaron;-kabina sa toplom vodom im stoji na raspolaganju, koji se nalazi u skolpu restorana<br />
-- Nakon izvlačenja ribe, riba se <strong>OBAVEZNO ODLAŽE NA VLAŽAN DU&Scaron;EK,</strong> tretira anti biotikom i nakon fotografisanja neozleđena vraća u vodu</p>
+<p>- Kako bi prijatan boravak i ponuda poseticima jezera bila kompletna, mogućnost kori&scaron;ćenja sanitarnog čvora i mogućnost tu&scaron;-kabina sa toplom vodom im stoji na raspolaganju, koji se nalazi u sklopu restorana.<br />
+- Nakon izvlačenja ribe, riba se <strong>OBAVEZNO ODLAŽE NA VLAŽAN DU&Scaron;EK,</strong> tretira antibiotikom i nakon fotografisanja neozleđena vraća u vodu</p>
 
 <p>&nbsp;</p>
 
@@ -38,10 +38,10 @@ Iz tog razloga su proistekle:</p>
 
 <p>&nbsp;</p>
 
-<p><strong>Naj strožije će se kazniti lice koje na bilo kakav način (slučajno ili namerno)ugrozi život ribe,svojim nepravilnim pona&scaron;anjem i odnosom prema samoj ribi.</strong><br />
+<p><strong>Najstrožije će se kazniti lice koje na bilo kakav način (slučajno ili namerno)ugrozi život ribe,svojim nepravilnim pona&scaron;anjem i odnosom prema samoj ribi.</strong><br />
 <br />
 - Sve ove odredbe su sačinjene iz razloga očuvanja ribljeg fonda i humanijeg pristupa sportskom ribolovu a i samoj prirodi koja nas okružuje.</p>
 
-<p>JEZERO TRESETI&Scaron;TE pruža nezaboravne trenutke u ribolovu, zato smo se pobrinuli kroz vi&scaron;egodi&scaron;nje iskustvo i težnjom za kvalitetnijom turističko-ribolovnom uslugom.</p>      
+<p>JEZERO TRESETI&Scaron;TE pruža nezaboravne trenutke u ribolovu, za to smo se pobrinuli kroz vi&scaron;egodi&scaron;nje iskustvo i težnjom za kvalitetnijom turističko-ribolovnom uslugom.</p>      
 
 @endsection
