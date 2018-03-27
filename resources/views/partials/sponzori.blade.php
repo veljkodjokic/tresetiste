82ed838fe9920acbf1383ed72f9857dec5dbc99e
veljkodjokic <veledjokic69@gmail.com>
2018-03-27 15:24:22 +0200

    dd

--- resources/views/partials/sponzori.blade.php
+++ resources/views/partials/sponzori.blade.php
@@ -1,4 +1,4 @@
-<a href="http://www.metalshopsu.rs/" target="_blank"><img src="/pics/metalshop.jpg" class="sponsor"></a>
+<a href="http://www.metalshopsu.rs/" target="_blank"><img src="/pics/metalshop.jpg" class="sponsor" style="border-radius: 2%"></a>
 <a href="https://www.facebook.com/tresetistesu" target="_blank"><img src="/pics/restoran.jpg" style="border-radius: 7%" class="sponsor"></a>
 <a href="https://www.topfish.rs/" target="_blank"><img src="/pics/topfish-logo.png" class="sponsor"></a>
 <a href="http://masterclean.rs/main/" target="_blank"><img src="/pics/master_clean.jpg" style="border-radius: 7%" class="sponsor"></a>
